6943879e456385e22e48c155085774a29552198f
vincentrodriguez <antoni.silvestrovic@gmail.com>
2018-08-02 11:24:33 +0300

    Initial Commit

---
+++ macOS airport tool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  macOS airport tool
+//
+//  Created by Antoni Silvestrovic on 02/08/2018.
+//  Copyright © 2018 Vincent Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
