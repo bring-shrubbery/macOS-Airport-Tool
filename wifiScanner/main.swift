755b1b1a14966e412c22a986421ae6a93b2c30d8
vincentrodriguez <antoni.silvestrovic@gmail.com>
2018-08-09 09:52:00 +0300

    minor decoration changes

--- wifiScanner/main.swift
+++ wifiScanner/main.swift
@@ -7,24 +7,31 @@
 //
 
 func printUsage() {
-    //get usage info and print it
+    // Get usage info and print it
     let usageInfo = UsageInfo()
     print(usageInfo.usageInfoData)
 }
 
 if(CommandLine.arguments.count < 2 ) {
+    // If user inputs no arguments (1 unique word in command line)
     printUsage()
 } else {
-    //setup scanner only if some arguments are provided
+    // User puts at least one argument after program name itself:
+    
+    // Setup scanner
     let scanner = Scanner()
     
-    if(CommandLine.arguments[1] == "-s") {
+    //a case of user requesting a scan:
+    if(CommandLine.arguments[1] == "-s" || CommandLine.arguments[1] == "scan" || CommandLine.arguments[1] == "--scan") {
         //standard scan argument
         scanner.scanNetworksClassic(nil)
     } else if(CommandLine.arguments.count < 4) {
         //standard scan with custom interface
         if(CommandLine.arguments[2] == "-s") {
             scanner.scanNetworksClassic(CommandLine.arguments[1])
+        } else {
+            // If no known commands are provided, or the syntax is incorrect - print usage info
+            printUsage()
         }
     }
 }
